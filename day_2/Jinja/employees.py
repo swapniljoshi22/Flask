0abd0b72e516e9bf0f7d92d09d3fc4429d8b076f
Swapnil Diliprao Joshi <72249773+swapniljoshi22@users.noreply.github.com>
2024-06-02 01:16:37 +0530

    Add files via upload

---
+++ day_2/Jinja/employees.py
@@ -0,0 +1,18 @@
+employees_data={
+    1:{
+        'name':"Swapnil",
+        'age':24,
+        'position':"ML Engineer"
+    },
+    2:{
+        'name':"Vrushabh",
+        'age':24,
+        'position':"Analyst"
+    },
+    3:{
+        'name':"Sahil",
+        'age':24,
+        'position':"dot_net dev"
+    }
+    
+}
